d88cb8398168b5fc68bbf050c0c7066b46ea977c
iammrdm <iammrdm@proton.me>
2023-12-14 15:48:19 +0800

    update scripts

--- bash-scripts/updateAllowedHosts.sh
+++ bash-scripts/updateAllowedHosts.sh
@@ -3,3 +3,5 @@
 getIP=`curl http://checkip.amazonaws.com`
 
 sed -i "s/change_me/${getIP}/g" /opt/pos-super-admin/api/settings.py
+
+bash /opt/devops-portfolio/bash-scripts/slackNotify.sh pos ${getIP}:8000 ${slack_webhook_url}
