7732fdf1c1573b9c1a9f83cc9ca3040018f2b51e
iammrdm <iammrdm@proton.me>
2023-12-14 17:15:08 +0800

    update

--- bash-scripts/updateAllowedHosts.sh
+++ bash-scripts/updateAllowedHosts.sh
@@ -5,17 +5,18 @@ source ~/.bash_profile
 
 
 getIP=`curl http://checkip.amazonaws.com`
-oldIP=`cat /tmp/oldIP.txt`
+oldIP=`cat /home/ec2-user/oldIP.txt`
 slack_webhook_url=`echo ${slack_webhook_url}`
 
-cat /tmp/publicIP.txt > /tmp/oldIP.txt
+cat /home/ec2-user/publicIP.txt > /home/ec2-user/oldIP.txt
 
 echo -e "[INFO] Current IP: ${getIP}"
 echo -e "[INFO] Old IP: ${oldIP}"
 
-echo ${getIP} > /tmp/publicIP.txt
+echo ${getIP} > /home/ec2-user/publicIP.txt
 
 sed -i "s/${oldIP}/${getIP}/g"  /home/ec2-user/pos-super-admin/api/settings.py
+sed -i "s/change_me/${getIP}/g"  /home/ec2-user/pos-super-admin/api/settings.py
 
 bash  /home/ec2-user/devops-portfolio/bash-scripts/runPythonBackground.sh
 sleep 5
