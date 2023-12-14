e5b2af9e750d2df42e19235f4ce086d406c2b9af
iammrdm <iammrdm@proton.me>
2023-12-14 17:17:12 +0800

    update

--- bash-scripts/updateAllowedHosts.sh
+++ bash-scripts/updateAllowedHosts.sh
@@ -3,12 +3,11 @@
 source ~/.bash_profile
 
 
+cat /home/ec2-user/publicIP.txt > /home/ec2-user/oldIP.txt
 
 getIP=`curl http://checkip.amazonaws.com`
 oldIP=`cat /home/ec2-user/oldIP.txt`
 slack_webhook_url=`echo ${slack_webhook_url}`
-
-cat /home/ec2-user/publicIP.txt > /home/ec2-user/oldIP.txt
 
 echo -e "[INFO] Current IP: ${getIP}"
 echo -e "[INFO] Old IP: ${oldIP}"
