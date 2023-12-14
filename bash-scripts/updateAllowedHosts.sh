b9deafd9af62bba879d448c4bb9fa6f951c4c77c
iammrdm <iammrdm@proton.me>
2023-12-14 16:02:54 +0800

    update script

--- bash-scripts/updateAllowedHosts.sh
+++ bash-scripts/updateAllowedHosts.sh
@@ -8,7 +8,11 @@ slack_webhook_url=`echo ${slack_webhook_url}`
 echo -e "[INFO] Current IP: ${getIP}"
 echo -e "[INFO] ${slack_webhook_url}"
 
+echo ${getIP} > /tmp/publicIP.txt
 
 sed -i "s/change_me/${getIP}/g" /opt/pos-super-admin/api/settings.py
+
+bash /opt/devops-portfolio/bash-scripts/runPythonBackground.sh
+sleep 5
 
 bash /opt/devops-portfolio/bash-scripts/slackNotify.sh pos ${getIP}:8000 ${slack_webhook_url}
