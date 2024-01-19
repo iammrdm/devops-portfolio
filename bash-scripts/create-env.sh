7827d046fb99bed7432585f2543ed801bc7e84e9
iammrdm <iammrdm@proton.me>
2024-01-19 15:37:26 +0800

    update create env

--- bash-scripts/create-env.sh
+++ bash-scripts/create-env.sh
@@ -12,13 +12,20 @@ apiOldIP=`cat /home/ec2-user/old-apiPublicIp.txt`
 slack_webhook_url=`echo ${slack_webhook_url}`
 getAPIPublicIPv4=`aws --region ap-southeast-1 ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=instance-id,Values=${apiInstanceID}" --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text`
 
+cd /home/ec2-user/pos-management && git reset --hard && git pull
+sleep 5
+
 echo "Management IP: ${managementIP}"
 echo "API old IP: ${apiOldIP}"
 echo "API new IP: ${getAPIPublicIPv4}"
 echo ${getAPIPublicIPv4}  > /home/ec2-user/apiPublicIp.txt
 
 sed -i "s/${apiOldIP}/${getAPIPublicIPv4}/g"  /etc/hosts
-sed -i "s/localhost/${getAPIPublicIPv4}/g"  /home/ec2-home/pos-management/docker-compose.yaml
+sed -i "s/localhost/${getAPIPublicIPv4}/g"  /home/ec2-user/pos-management/docker-compose.yaml
+sleep 5
+
+cd /home/ec2-user/pos-management && docker build -t iammrdm/laundry-pos:management-prod-latest .
+sleep 5
 
 bash  /home/ec2-user/devops-portfolio/bash-scripts/slackNotify.sh frontend management ${managementIP}:8081 ${slack_webhook_url}
 
